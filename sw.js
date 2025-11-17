045b25da1acb5454a77a139bc1bf0d34c60c568e
Nur Aishah <nuraishah.zulaikah@gmail.com>
2025-11-17 11:38:23 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,1 @@
-// simple service worker – minimal footprint
-self.addEventListener('install', function(e){ self.skipWaiting(); });
-self.addEventListener('activate', function(e){ self.clients.claim(); });
-self.addEventListener('fetch', function(e){ /* network pass-through */ });
+self.addEventListener("fetch", () => {});
